cc9b3ff441a89d1ccde96f8bb3a472fcf72d1cb6
s.pirohov <sergey.pirogov@gmail.com>
2022-08-26 14:30:08 +0300

    fix env vars

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import express from 'express';
 import helmet from 'helmet';
 import hpp from 'hpp';
 import morgan from 'morgan';
-import { NODE_ENV, PORT, LOG_FORMAT, ORIGIN, CREDENTIALS } from './config';
+import { NODE_ENV, PORT, LOG_FORMAT } from './config';
 import { Routes } from './interfaces/routes.interface';
 import errorMiddleware from './middlewares/error.middleware';
 import { logger, stream } from './utils/logger';
@@ -39,7 +39,7 @@ class App {
 
   private initializeMiddlewares() {
     this.app.use(morgan(LOG_FORMAT, { stream }));
-    this.app.use(cors({ origin: ORIGIN, credentials: CREDENTIALS }));
+    this.app.use(cors());
     this.app.use(hpp());
     this.app.use(helmet());
     this.app.use(compression());
--- src/config/index.ts
+++ src/config/index.ts
@@ -2,6 +2,8 @@ import { config } from 'dotenv';
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
-export const { NODE_ENV, PORT, SECRET_KEY, LOG_FORMAT, ORIGIN } = process.env;
+export const { NODE_ENV } = process.env;
 export const MOCKS_DIR = './mocks';
-export const LOG_DIR = './logs'
+export const PORT = process.env.PORT || 3000;
+export const LOG_DIR = '../../logs';
+export const LOG_FORMAT = 'combined';
