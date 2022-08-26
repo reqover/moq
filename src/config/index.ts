c16fd2341b76b62318b7c420e6be4496cb95c1a2
s.pirohov <sergey.pirogov@gmail.com>
2022-08-26 14:14:08 +0300

    fix log dir

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,5 +2,6 @@ import { config } from 'dotenv';
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
-export const { NODE_ENV, PORT, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN } = process.env;
+export const { NODE_ENV, PORT, SECRET_KEY, LOG_FORMAT, ORIGIN } = process.env;
 export const MOCKS_DIR = './mocks';
+export const LOG_DIR = './logs'
